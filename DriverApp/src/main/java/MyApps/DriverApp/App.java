c0de04befc2788827a3416c0f3990ef14e8e8370
ChitreshSirohi <chitresh.sirohi@gmail.com>
2016-11-22 12:48:55 +0530

    First Commit

---
+++ DriverApp/src/main/java/MyApps/DriverApp/App.java
@@ -0,0 +1,13 @@
+package MyApps.DriverApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
